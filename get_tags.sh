ec2f569028aeb8ae61f129cba4e9b8e992c093c3
EC2 Default User <ec2-user@ip-10-127-248-187.ec2.internal>
2017-09-27 14:53:15 +0000

    cleaned up stuff.

--- get_tags.sh
+++ get_tags.sh
@@ -13,16 +13,15 @@ DATE=$(date '+%D%M%Y')
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document/ | grep -i region | awk -F \" '{print $4}')
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document/ | grep -i instanceid | awk -F \" '{print $4}')
 
-set_label(){
-   TAG_VALUE=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$1" --region=$REGION --output=text | cut -f5)
-   return $TAG_VALUE
+show_value(){
+   echo $(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$1" --region=$REGION --output=text | cut -f5)
 }
 
 while getopts ":t:" opt; 
 do
    case $opt in
      t)
-         TAG=$OPTARG
+         show_value $OPTARG
          ;;
      /?)
          echo "Unknow option." >&2
@@ -34,4 +33,3 @@ do
          ;;
    esac
 done
-echo set_label $TAG
