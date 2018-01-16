c222ce55408703fce4abe48da560b538e580d3a9
Richard Hollenbeck <demonic@davek-73access.inside.rci>
2018-01-16 14:33:37 -0500

    Cleaned up old files.

--- eip_report.py
+++ eip_report.py
@@ -4,18 +4,18 @@ import sys
 import boto3
 
 def find_unused_eip():
-    client = boto3.client("ec2")
-    regions = client.describe_regions()
-
+    s = boto3.Session(profile_name="prod", region_name="us-east-1")
+    c = s.client("ec2")
+    regions = c.describe_regions()
     for region in regions["Regions"]:
         session = boto3.Session(profile_name="prod", region_name=region["RegionName"])
         client = session.client("ec2")
         addresses = client.describe_addresses()
         if addresses["Addresses"]:
-            print "Elastic IPs for {0}".format(region["RegionName"])
+            print "Checking for unallocated Elastic IPs in {0}".format(region["RegionName"])
             for eip_dict in addresses["Addresses"]:
-                print "[*] Public IP:{0}, Association Id: {1}, Instance Id: {2}".format(eip_dict["PublicIp"], eip_dict["AssociationId"], eip_dict["InstanceId"])
-            print "\n"
+                if not eip_dict.has_key("InstanceId"):
+                    print "[*] Unused Elastic IP - Public IP: {0}".format(eip_dict["PublicIp"])
 
 def main():
     find_unused_eip()
--- get_untagged_resources.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python  
-
-import boto3
-
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-print response
