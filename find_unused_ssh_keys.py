a1a0e6a16b08e34cc1158e8158f67cd83d25b99b
ro1423 <ro1423@email.net>
2020-05-12 08:52:24 -0400

    updated libraries

--- find_unused_ssh_keys.py
+++ find_unused_ssh_keys.py
@@ -27,28 +27,30 @@ def main():
     regions = c.describe_regions()
     dtg = date.today().strftime("%d_%m_%Y")
     with open("unused_ssh_key_report_" + dtg + ".txt", "w") as txtfile:        
-        for region in regions["Regions"]:
-            session = boto3.Session(profile_name="prod", region_name=region["RegionName"])
-            ec2 = session.client("ec2")
-            all_key_pairs = ec2.describe_key_pairs()
-            all_instances = ec2.describe_instances()
-            instance_key_list = set()
-            key_list = set()
-            
-            txtfile.write(region["RegionName"] + '\n')
-            for reservation in all_instances["Reservations"]:
-                for instance in reservation["Instances"]:
-                    instance_key_list.add(instance["KeyName"])
-            for key_pair in all_key_pairs["KeyPairs"]:
-                key_list.add(key_pair["KeyName"])
-                print key_pair["KeyName"]
-            unused_keys = key_list - instance_key_list
+        try:
+		for region in regions["Regions"]:
+		    session = boto3.Session(profile_name="prod", region_name=region["RegionName"])
+		    ec2 = session.client("ec2")
+		    all_key_pairs = ec2.describe_key_pairs()
+		    all_instances = ec2.describe_instances()
+		    instance_key_list = set()
+		    key_list = set()
+		    
+		    txtfile.write(region["RegionName"] + '\n')
+		    for reservation in all_instances["Reservations"]:
+			for instance in reservation["Instances"]:
+			    instance_key_list.add(instance["KeyName"])
+		    for key_pair in all_key_pairs["KeyPairs"]:
+			key_list.add(key_pair["KeyName"])
+			print key_pair["KeyName"]
+		    unused_keys = key_list - instance_key_list
 
-            #if unused_keys:
-                #delete_keys(ec2=ec2, unused_keys=unused_keys)
-            #    txtfile.write(','.join(unused_keys))
-            #txtfile.write('\n')
+		    #if unused_keys:
+			#delete_keys(ec2=ec2, unused_keys=unused_keys)
+		    #    txtfile.write(','.join(unused_keys))
+		    #txtfile.write('\n')
                         
-
+        except ClientError as e:
+            pass
 if __name__ == "__main__":
     sys.exit(main())
